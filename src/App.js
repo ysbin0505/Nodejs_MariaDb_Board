cc31efd428fe916cb4c7da7198094ba7ef815114
ysbin0505@naver.com <118337787+ysbin0505@users.noreply.github.com>
2023-05-22 20:58:07 +0900

    react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import {Button, Progress} from 'semantic-ui-react'
+
+function App() {
+  return (
+      <div>
+    <p> 안녕하세요</p>
+    <Button primary> 하이하이 </Button>
+    <Progress precent = {66} />
+    </div>
+  );
+}
+
+export default App;
