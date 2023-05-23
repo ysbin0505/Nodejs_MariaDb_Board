c05fb114a49b3dba9ca0d8400801e234c05721c2
ysbin0505@naver.com <118337787+ysbin0505@users.noreply.github.com>
2023-05-23 19:38:36 +0900

    html

--- nodeserver/server.js
+++ nodeserver/server.js
@@ -1,16 +1,22 @@
-// 1. 서버 사용을 위해서 http 모듈을 http 변수에 담는다. (모듈과 변수의 이름은 달라도 된다.)
+const path = require('path');
 const http = require('http');
+const fs = require('fs');
 
-// 2. http 모듈로 서버를 생성한다.
-//    아래와 같이 작성하면 서버를 생성한 후, 사용자로 부터 http 요청이 들어오면 function 블럭내부의 코드를 실행해서 응답한다.
-const server = http.createServer(function(request,response){
+const server = http.createServer(function(request, response) {
+  const filePath = path.join(__dirname, 'html', 'index2.html');
 
-    response.writeHead(200,{'Content-Type':'text/html'});
-    response.end('Yu seung Bin Dbms Subject~!!');
+  fs.readFile(filePath, 'utf8', function(err, data) {
+    if (err) {
+      response.writeHead(500, { 'Content-Type': 'text/plain' });
+      response.end('Internal Server Error');
+      return;
+    }
 
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.end(data);
+  });
 });
 
-// 3. listen 함수로 3000 포트를 가진 서버를 실행한다. 서버가 실행된 것을 콘솔창에서 확인하기 위해 'Server is running...' 로그를 출력한다
-server.listen(3000, function(){
-    console.log('Server is running...');
+server.listen(3000, function() {
+  console.log('Server is running...');
 });
