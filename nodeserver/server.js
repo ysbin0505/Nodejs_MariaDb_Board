c15d7646f60e21571c62f3050ac124be392ec69d
ysbin0505@naver.com <118337787+ysbin0505@users.noreply.github.com>
2023-05-23 16:41:42 +0900

    Update server.js

--- nodeserver/server.js
+++ nodeserver/server.js
@@ -10,7 +10,7 @@ var server = http.createServer(function(request,response){
 
 });
 
-// 3. listen 함수로 8080 포트를 가진 서버를 실행한다. 서버가 실행된 것을 콘솔창에서 확인하기 위해 'Server is running...' 로그를 출력한다
+// 3. listen 함수로 3000 포트를 가진 서버를 실행한다. 서버가 실행된 것을 콘솔창에서 확인하기 위해 'Server is running...' 로그를 출력한다
 server.listen(3000, function(){
     console.log('Server is running...');
 });
