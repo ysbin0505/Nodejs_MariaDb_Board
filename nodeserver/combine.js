dec6ce52749181a2bb56d4811790f454514cc2e0
ysbin0505@naver.com <118337787+ysbin0505@users.noreply.github.com>
2023-05-30 14:20:34 +0900

    d

--- nodeserver/combine.js
+++ nodeserver/combine.js
@@ -25,22 +25,77 @@ const pool = mariadb.createPool({
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/add', async (req, res) => {
+  // 게시물 정보 가져오기
   const title = req.body.title;
   const content = req.body.content;
   const writer = req.body.writer;
 
   let conn;
-  try {
+  try { //작성한 내용 db로 삽입
     conn = await pool.getConnection();
     await conn.query('USE nodejs_test');
     await conn.query('INSERT INTO tbl_board (title, content, writer) VALUES (?, ?, ?)', [title, content, writer]);
-    res.redirect('/index.html'); // Redirect to index.html
+    res.redirect('/index.html'); // index.html로 리디렉션
   } catch (err) {
     res.status(500).send('글 작성 중 오류가 발생했습니다.');
   } finally {
     if (conn) conn.end();
   }
 });
+
+app.get('/', (req, res) => {
+  res.redirect('/index.html'); // index.html로 리디렉션
+});
+
+app.get('/index.html', async (req, res) => {  //db내용 index.html로 삽입
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    await conn.query('USE nodejs_test');
+    const results = await conn.query('SELECT num, title, writer, date FROM tbl_board');
+    const html = renderIndexPage(results);
+    res.send(html);
+  } catch (err) {
+    res.status(500).send('내부 서버 오류');
+  } finally {
+    if (conn) conn.end();
+  }
+});
+
+function renderIndexPage(posts) {   //index.html
+  let tableRows = '';
+  posts.forEach(post => {
+    tableRows += `
+      <tr>
+        <td>${post.num}</td>
+        <td><a href="/posts/${post.num}">${post.title}</a></td>
+        <td>${post.writer}</td>
+        <td>${post.date}</td>
+      </tr>
+    `;
+  });
+
+  const html = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8');
+  return html.replace('<!-- 동적으로 생성된 테이블 행 -->', tableRows);
+}
+
+function renderIndexPage2(posts) {    //content.html
+  let tableRows = '';
+  posts.forEach(post => {
+    tableRows += `
+      <tr>
+        <td>${post.num}</td>
+        <td><a href="/posts/${post.num}">${post.title}</a></td>
+        <td>${post.content}</td>
+        <td>${post.writer}</td>
+        <td>${post.date}</td>
+      </tr>
+    `;
+  });
+
+  const html = fs.readFileSync(path.join(__dirname, 'content.html'), 'utf8');
+  return html.replace('<!-- 동적으로 생성된 테이블 행 -->', tableRows);
+}
 
 app.get('/index.html', (req, res) => {
   const filePath = path.join(__dirname, 'index.html');
@@ -64,8 +119,8 @@ pool.getConnection()
 
 const serverPort = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  const filePath = path.join(__dirname, 'index.html');
+app.get('/write.html', (req, res) => {
+  const filePath = path.join(__dirname, 'write.html');
   fs.readFile(filePath, 'utf8', function (err, data) {
     if (err) {
       res.status(500).send('Internal Server Error');
@@ -75,8 +130,8 @@ app.get('/', (req, res) => {
   });
 });
 
-app.get('/write.html', (req, res) => {
-  const filePath = path.join(__dirname, 'write.html');
+app.get('/content.html', (req, res) => {
+  const filePath = path.join(__dirname, 'content.html');
   fs.readFile(filePath, 'utf8', function (err, data) {
     if (err) {
       res.status(500).send('Internal Server Error');
