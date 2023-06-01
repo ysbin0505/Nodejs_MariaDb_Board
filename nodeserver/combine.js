c1c8d9b2a9c228ea9226d7aec9cfdf8c2a2b9467
ysbin0505@naver.com <118337787+ysbin0505@users.noreply.github.com>
2023-06-01 23:53:28 +0900

    댓글까지 완료

--- nodeserver/combine.js
+++ nodeserver/combine.js
@@ -1,16 +1,16 @@
-const path = require('path');
-const http = require('http');
-const fs = require('fs');
-const mariadb = require('mariadb');
-const express = require('express');
+const path = require("path");
+const http = require("http");
+const fs = require("fs");
+const mariadb = require("mariadb");
+const express = require("express");
 
 const app = express();
 const server = http.createServer(app);
 
-const DBHost = '127.0.0.1';
-const DBPort = '3306';
-const DBUser = 'root';
-const DBPass = 'qls515';
+const DBHost = "127.0.0.1";
+const DBPort = "3306";
+const DBUser = "root";
+const DBPass = "qls515";
 const connectionLimit = 5;
 
 const pool = mariadb.createPool({
@@ -18,13 +18,16 @@ const pool = mariadb.createPool({
   port: DBPort,
   user: DBUser,
   password: DBPass,
-  connectionLimit: connectionLimit
+  connectionLimit: connectionLimit,
+  database: "nodejs_test",
+  bigIntAsNumber: true,
 });
 
 // body-parser middleware
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.post('/add', async (req, res) => {
+app.post("/add", async (req, res) => {
   // 게시물 정보 가져오기
   const title = req.body.title;
   const content = req.body.content;
@@ -33,91 +36,51 @@ app.post('/add', async (req, res) => {
   let conn;
   try {
     conn = await pool.getConnection();
-    await conn.query('USE nodejs_test');
 
     // 게시물 저장
-    await conn.query('INSERT INTO tbl_board (title, content, writer) VALUES (?, ?, ?)', [title, content, writer]);
+    await conn.query(
+      "INSERT INTO tbl_board (title, content, writer) VALUES (?, ?, ?)",
+      [title, content, writer]
+    );
 
     // 저장된 게시물의 num 가져오기
-    const result = await conn.query('SELECT num FROM tbl_board WHERE title = ? AND content = ? AND writer = ?', [title, content, writer]);
+    const result = await conn.query(
+      "SELECT num FROM tbl_board WHERE title = ? AND content = ? AND writer = ?",
+      [title, content, writer]
+    );
     const postId = result[0].num;
 
-    res.redirect('/index.html'); // index.html로 리디렉션
+    res.redirect("/index.html"); // index.html로 리디렉션
   } catch (err) {
-    res.status(500).send('글 작성 중 오류가 발생했습니다.');
+    res.status(500).send("글 작성 중 오류가 발생했습니다.");
   } finally {
     if (conn) conn.end();
   }
 });
 
-app.post('/comment/:id', async (req, res) => {
-  const postId = req.params.id;
-  const commentWriter = req.body.writer;
-  const commentContent = req.body.content;
+app.get("/", (req, res) => {
+  res.redirect("/index.html"); // index.html로 리디렉션
+});
 
+app.get("/index.html", async (req, res) => {
   let conn;
   try {
     conn = await pool.getConnection();
-    await conn.query('USE nodejs_test');
-
-    // 댓글 저장
-    await conn.query('INSERT INTO comment (c_writer, c_content, post_id) VALUES (?, ?, ?)', [commentWriter, commentContent, postId]);
-
-    res.sendStatus(200); // Success status
-    res.redirect('/index.html'); // index.html로 리디렉션
-  } catch (err) {
-    console.error('댓글 작성 중 오류:', err);
-    res.sendStatus(500); // Internal server error status
-  } finally {
-    if (conn) conn.end();
-  }
-});
-
-
-app.get('/comments/:id', async (req, res) => {
-  const postId = req.params.id;
-
-  let conn;
-  try {
-    conn = await pool.getConnection();
-    await conn.query('USE nodejs_test');
-    const results = await conn.query('SELECT * FROM comment WHERE post_id = ?', [postId]);
-
-    res.json({ comments: results }); // JSON response with comments array
-
-  } catch (err) {
-    console.error('댓글 가져오기 오류:', err);
-    res.sendStatus(500); // Internal server error status
-  } finally {
-    if (conn) conn.end();
-  }
-});
-
-
-
-
-app.get('/', (req, res) => {
-  res.redirect('/index.html'); // index.html로 리디렉션
-});
-
-app.get('/index.html', async (req, res) => {
-  let conn;
-  try {
-    conn = await pool.getConnection();
-    await conn.query('USE nodejs_test');
-    const results = await conn.query('SELECT num, title, writer, date FROM tbl_board');
+    const results = await conn.query(
+      "SELECT num, title, writer, date FROM tbl_board"
+    );
     const html = renderIndexTable(results);
     res.send(html);
   } catch (err) {
-    res.status(500).send('내부 서버 오류');
+    res.status(500).send("내부 서버 오류");
   } finally {
     if (conn) conn.end();
   }
 });
 
 function renderIndexTable(posts, currentPage, totalPages) {
-  let tableRows = '';
-  posts.forEach(post => {
+  let tableRows = "";
+  posts.forEach((post) => {
     tableRows += `
       <tr>
         <td>${post.num}</td>
@@ -128,13 +91,15 @@ function renderIndexTable(posts, currentPage, totalPages) {
     `;
   });
 
-  const html = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8');
+  const html = fs.readFileSync(path.join(__dirname, "index.html"), "utf8");
   const pagination = renderPagination(currentPage, totalPages);
-  return html.replace('<!-- 동적으로 생성된 테이블 행 -->', tableRows).replace('<!-- 페이징 -->', pagination);
+  return html
+    .replace("<!-- 동적으로 생성된 테이블 행 -->", tableRows)
+    .replace("<!-- 페이징 -->", pagination);
 }
 
 function renderPagination(currentPage, totalPages) {
-  let paginationHTML = '';
+  let paginationHTML = "";
   for (let i = 1; i <= totalPages; i++) {
     if (i === currentPage) {
       paginationHTML += `<span>${i}</span>`;
@@ -146,93 +111,201 @@ function renderPagination(currentPage, totalPages) {
   return paginationHTML;
 }
 
-pool.getConnection()
-  .then(conn => {
-    console.log('데이터베이스 연결 성공');
+pool
+  .getConnection()
+  .then((conn) => {
+    console.log("데이터베이스 연결 성공");
     conn.release();
   })
-  .catch(err => {
-    console.error('데이터베이스 연결 오류:', err);
+  .catch((err) => {
+    console.error("데이터베이스 연결 오류:", err);
   });
 
 const serverPort = process.env.PORT || 3000;
 
-app.get('/write.html', (req, res) => {
-  const filePath = path.join(__dirname, 'write.html');
-  fs.readFile(filePath, 'utf8', function (err, data) {
+app.get("/write.html", (req, res) => {
+  const filePath = path.join(__dirname, "write.html");
+  fs.readFile(filePath, "utf8", function (err, data) {
     if (err) {
-      res.status(500).send('Internal Server Error');
+      res.status(500).send("Internal Server Error");
       return;
     }
     res.send(data);
   });
 });
 
+// 게시글 조회 API 엔드포인트
+app.get("/contents", async (req, res) => {
+  const { page = 1, limit = 10 } = req.query;
+
+  const offset = (Number(page) - 1) * limit;
+
+  let conn;
+  try {
+    conn = await pool.getConnection();
+
+    // 게시글 조회 쿼리 실행
+    const query = `SELECT * FROM tbl_board ORDER BY num DESC LIMIT ? OFFSET ?`;
+    const rows = await conn.query(query, [Number(limit), offset]);
+
+    // 전체 게시글 수 조회 쿼리 실행
+    const countQuery = "SELECT COUNT(*) as total FROM tbl_board";
+    const [{ total }] = await conn.query(countQuery);
+
+    // 결과 반환
+    res.json({
+      total,
+      offset: Number(offset),
+      limit: Number(limit),
+      data: rows,
+    });
+  } catch (err) {
+    console.error("Error during database query:", err);
+    res.status(500).send("Internal Server Error");
+  } finally {
+    if (conn) {
+      // 연결 반환
+      conn.release();
+    }
+  }
+});
+
 // 게시물 API 엔드포인트
-app.get('/content/:id', async (req, res) => {
+app.get("/content/:id", async (req, res) => {
   const postId = req.params.id;
 
   let conn;
   try {
     conn = await pool.getConnection();
-    await conn.query('USE nodejs_test');
-    const results = await conn.query('SELECT num, title, content, writer, date FROM tbl_board WHERE num = ?', [postId]);
+    const results = await conn.query(
+      "SELECT num, title, content, writer, date FROM tbl_board WHERE num = ?",
+      [postId]
+    );
 
     if (results.length === 0) {
-      res.status(404).send('게시글을 찾을 수 없습니다.');
+      res.status(404).send("게시글을 찾을 수 없습니다.");
       return;
     }
 
     const post = results[0];
 
     res.json({ post }); // JSON 형식으로 게시물 데이터 응답
-
   } catch (err) {
-    console.error('게시물 가져오기 오류:', err);
-    res.status(500).send('내부 서버 오류');
+    console.error("게시물 가져오기 오류:", err);
+    res.status(500).send("내부 서버 오류");
   } finally {
     if (conn) conn.end();
   }
 });
 
+// 댓글 작성 API
+app.post("/contents/:id/comments", async (req, res) => {
+  const articleId = req.params.id;
+  const { writer, content } = req.body;
 
-app.get('/content.html', (req, res) => {
-  const filePath = path.join(__dirname, 'content.html');
-  fs.readFile(filePath, 'utf8', function (err, data) {
+  try {
+    const conn = await pool.getConnection();
+
+    const query = `
+      INSERT INTO comment (c_writer, c_content, post_id)
+      VALUES (?, ?, ?)
+    `;
+    const values = [writer, content, articleId];
+
+    await conn.query(query, values);
+
+    conn.release();
+
+    res.status(201).json({ message: "댓글이 작성되었습니다." });
+  } catch (error) {
+    console.error("댓글 작성 중 오류 발생:", error);
+    res.status(500).json({ error: "서버 오류" });
+  }
+});
+
+// 댓글 조회 API
+app.get("/contents/:id/comments", async (req, res) => {
+  const articleId = req.params.id;
+
+  try {
+    const conn = await pool.getConnection();
+
+    const query = `
+      SELECT id, c_writer as writer, c_content as content, c_date as writtenAt
+      FROM comment
+      WHERE post_id = ?
+      ORDER BY c_date DESC
+    `;
+    const values = [articleId];
+
+    const rows = await conn.query(query, values);
+
+    conn.release();
+
+    res.status(200).json(rows);
+  } catch (error) {
+    console.error("댓글 조회 중 오류 발생:", error);
+    res.status(500).json({ error: "서버 오류" });
+  }
+});
+
+// 댓글 삭제 API
+app.delete("/comments/:id", async (req, res) => {
+  const commentId = req.params.id;
+
+  try {
+    const conn = await pool.getConnection();
+
+    const query = `
+      DELETE FROM comment
+      WHERE id = ?
+    `;
+    const values = [commentId];
+
+    await conn.query(query, values);
+
+    conn.release();
+
+    res.status(200).json({ message: "댓글이 삭제되었습니다." });
+  } catch (error) {
+    console.error("댓글 삭제 중 오류 발생:", error);
+    res.status(500).json({ error: "서버 오류" });
+  }
+});
+
+app.get("/content.html", (req, res) => {
+  const filePath = path.join(__dirname, "content.html");
+  fs.readFile(filePath, "utf8", function (err, data) {
     if (err) {
-      res.status(500).send('Internal Server Error');
+      res.status(500).send("Internal Server Error");
       return;
     }
     res.send(data);
   });
 });
 
-
-app.get('/post/:id', async (req, res) => {
+app.get("/post/:id", async (req, res) => {
   const postId = req.params.id;
   res.redirect(`/content.html?id=${postId}`);
 });
 
-app.delete('/delete/:id', async (req, res) => {
+app.delete("/delete/:id", async (req, res) => {
   const postId = req.params.id;
 
   let conn;
   try {
     conn = await pool.getConnection();
-    await conn.query('USE nodejs_test');
-    await conn.query('DELETE FROM tbl_board WHERE num = ?', [postId]);
+    await conn.query("DELETE FROM tbl_board WHERE num = ?", [postId]);
 
-    res.json({ message: '게시물이 삭제되었습니다.' });
-
+    res.json({ message: "게시물이 삭제되었습니다." });
   } catch (err) {
-    console.error('게시물 삭제 오류:', err);
-    res.status(500).json({ error: '게시물 삭제 중 오류가 발생했습니다.' });
+    console.error("게시물 삭제 오류:", err);
+    res.status(500).json({ error: "게시물 삭제 중 오류가 발생했습니다." });
   } finally {
     if (conn) conn.end();
   }
 });
 
-
 server.listen(serverPort, () => {
-  console.log('Server 작동 중...');
+  console.log("Server 작동 중...");
 });
